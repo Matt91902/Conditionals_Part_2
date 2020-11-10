3526c68c5d82bba9bf693c67f245cf0d6727ecdc
Matt Young <matt02young@gmai.com>
2020-11-10 13:27:04 -0500

    Middle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,9 +10,72 @@ public class Main {
         }
         else return false;
     }
-    public static boolean isLater(int r, int g, int b)
+
+    public static boolean isLater(int month1, int day1, int year1, int month2, int day2, int year2)
     {
-        
+        if (year1 > year2)
+        {
+            return true;
+        }
+        else if (year1 == year2 && month1 > month2)
+        {
+            return true;
+        }
+        else if (year1 == year2 && month1 == month2 && day1 > day2)
+        {
+            return true;
+        }
+        else return false;
     }
 
+    public static String bestMatch(int r, int g, int b)
+    {
+        if (r == g && r == b)
+        {
+            return "GRAY";
+        }
+        else if (r == g && r > b)
+        {
+            return "YELLOW";
+        }
+        else if (r == b && r > g)
+        {
+            return "MAGENTA";
+        }
+        else if (g == b && g > r)
+        {
+            return "CYAN";
+        }
+        else if (r > g && r > b)
+        {
+            return "RED";
+        }
+        else if (g > r && g > b)
+        {
+            return "GREEN";
+        }
+        else if (b > r && b > g)
+        {
+            return "BLUE";
+        }
+        else return "None";
+    }
+
+    public static int findBestFit(int size1, int size2, int space)
+    {
+        int x = 0;
+        if (size1 + size2 <= space)
+        {
+            x = 3;
+        }
+        else if (size1 + size2 > space && size1 > size2 )
+        {
+            x = 1;
+        }
+        else if (size1 + size2 > space && size2 > size1)
+        {
+            x = 2;
+        }
+        return x;
+    }
 }
