a24285f3c6517da545c9e5cf0f3e3927feaf07c2
Matt Young <matt02young@gmai.com>
2020-11-11 12:35:01 -0500

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,48 @@ package com.company;
 
 public class Main {
 
+    public static void main(String[] args) {
+//leap year
+        System.out.println("2000 " + isLeapYear(2000));
+        System.out.println("2004 " + isLeapYear(2004));
+        System.out.println("2003 " + isLeapYear(2003));
+        System.out.println("2100 " + isLeapYear(2100));
+// is later
+        System.out.println("1/2/2010 is later than 1/2/2011 " + isLater(1,2,2010, 1,2,2011));
+        System.out.println("1/2/2011 is later than 1/2/2010 " + isLater(1,2,2011, 1,2,2010));
+        System.out.println("1/5/2010 is later than 3/2/2010 " + isLater(1,5,2010, 3,2,2010));
+        System.out.println("3/2/2010 is later than 1/2/2010 " + isLater(3,2,2010, 1,2,2010));
+        System.out.println("1/3/2010 is later than 1/2/2010 " + isLater(1,3,2010, 1,2,2010));
+        System.out.println("1/2/2010 is later than 1/3/2010 " + isLater(1,2,2010, 1,3,2010));
+        System.out.println("1/2/2010 is later than 1/2/2010 " + isLater(1,2,2010, 1,2,2010));
+        System.out.println("4/8/2010 is later than 1/2/2011 " + isLater(4,8,2010, 1,2,2011));
+
+// Best match
+        System.out.println("Best match 2 3 4 " + bestMatch(2,3,4));
+        System.out.println("Best match 4 3 3 " + bestMatch(4,3,3));
+        System.out.println("Best match 3 8 4 " + bestMatch(3,8,4));
+        System.out.println("Best match 4 3 4 " + bestMatch(4,3,4));
+        System.out.println("Best match 2 4 4 " + bestMatch(2,4,4));
+        System.out.println("Best match 8 8 4 " + bestMatch(8,8,4));
+        System.out.println("Best match 4 4 4 " + bestMatch(4,4,4));
+// Best Fit
+        System.out.println("Find Best fit 2 3 6 is " + findBestFit(2,3,6));
+        System.out.println("Find Best fit 4 3 6 is " + findBestFit(4,3,6));
+        System.out.println("Find Best fit 3 4 6 is " + findBestFit(3,4,6));
+        System.out.println("Find Best fit 2 3 1 is " + findBestFit(2,3,1));
+        System.out.println("Find Best fit 6 3 4 is " + findBestFit(6,3,4));
+        System.out.println("Find Best fit 3 6 4 is " + findBestFit(3,6,4));
+// Make Bench
+        System.out.println("Make bench 3 1 8 is " + makeBench(3,1,8));
+        System.out.println("Make bench 3 1 9 is " + makeBench(3,1,9));
+        System.out.println("Make bench 11 1 15 is " + makeBench(11,1,15));
+        System.out.println("Make bench 4 2 15 is " + makeBench(4,2,15));
+        System.out.println("Make bench 20 0 20 is " + makeBench(20,0,20));
+        System.out.println("Make bench 3 4 20 is " + makeBench(3,4,20));
+        System.out.println("Make bench 0 6 30 is " + makeBench(0,6,30));
+        System.out.println("Make bench 0 5 30 is " + makeBench(0,5,30));
+    }
+
     public static boolean isLeapYear(int year)
     {
         if (year%4 == 0)
@@ -68,14 +110,32 @@ public class Main {
         {
             x = 3;
         }
-        else if (size1 + size2 > space && size1 > size2 )
+        else if (size1 > space && size2 > space)
+        {
+            x = 0;
+        }
+        else if (size1 > space && size2 < space)
         {
             x = 1;
         }
-        else if (size1 + size2 > space && size2 > size1)
+        else if (size1 < space && size2 > space)
         {
             x = 2;
         }
+        else if (size1 < space && size2 < space && size1 >= size2)
+        {
+            x = 1;
+        }
+        else x = 2;
         return x;
+    }
+
+    public static boolean makeBench(int small, int big, int goal)
+    {
+        if (small + 5*big >= goal)
+        {
+            return true;
+        }
+        else return false;
     }
 }
